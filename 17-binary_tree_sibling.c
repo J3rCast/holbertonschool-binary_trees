dcb800dfed0f7d52653dc5da7f34f1f9614bd016
J3rCast <jerson.castro.r@gmail.com>
2022-07-28 10:18:24 -0500

    task 17 added

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,10 +11,10 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (node->parent->left->n == node->n)
+	if (node->parent->left == node)
 		return (node->parent->right);
 
-	if (node->parent->right->n == node->n)
+	if (node->parent->right == node)
 		return (node->parent->left);
 
 	return (node);
